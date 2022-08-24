335f7735ff6fab1c14a84a0a2c6aee5dd9f4679e
Mocking <497773732@qq.com>
2022-08-24 21:19:26 +0800

    1.0.2

--- src/main/main.js
+++ src/main/main.js
@@ -2,7 +2,7 @@
  * @Author: Mocking 497773732@qq.com
  * @Date: 2022-08-24 10:10:53
  * @LastEditors: Mocking 497773732@qq.com
- * @LastEditTime: 2022-08-24 20:40:46
+ * @LastEditTime: 2022-08-24 21:12:47
  * @FilePath: \electron\src\main\main.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
